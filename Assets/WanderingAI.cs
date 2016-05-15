310f854b7a5f315626731d3820882ed35fccd25f
Andrew Bell <fiixed@gmail.com>
2016-05-15 14:55:15 -0500

    added fireballs

--- Assets/WanderingAI.cs
+++ Assets/WanderingAI.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class WanderingAI : MonoBehaviour {
 
+	[SerializeField] private GameObject fireballPrefab;
+	private GameObject _fireball;
+
 	private bool _alive;
 
 	public float speed = 3.0f;
@@ -20,7 +23,15 @@ public class WanderingAI : MonoBehaviour {
 			Ray ray = new Ray(transform.position, transform.forward);
 			RaycastHit hit;
 			if (Physics.SphereCast(ray, 0.75f, out hit)) {
-				if (hit.distance < obstacleRange) {
+				GameObject hitObject = hit.transform.gameObject;
+				if (hitObject.GetComponent<PlayerCharacter>()) {
+					if (_fireball == null) {
+						_fireball = Instantiate(fireballPrefab) as GameObject;
+						_fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+						_fireball.transform.rotation = transform.rotation;
+					}
+
+				} else if (hit.distance < obstacleRange) {
 					float angle = Random.Range(-110, 110);
 					transform.Rotate(0, angle, 0);
 				}
