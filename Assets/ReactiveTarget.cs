3e2542e45d9de995f2e27837bfc155abddfc6fae
Andrew Bell <fiixed@gmail.com>
2016-05-15 13:20:51 -0500

    first commit

---
+++ Assets/ReactiveTarget.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReactiveTarget : MonoBehaviour {
+
+	// Use this for initialization
+	public void ReactToHit() {
+		StartCoroutine(Die());
+	}
+	
+	private IEnumerator Die() {
+		this.transform.Rotate(-75, 0, 0);
+
+		yield return new WaitForSeconds(1.5f);
+
+		Destroy(this.gameObject);
+	}
+}
